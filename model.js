f3c467ac54ebf5bf147274d1c5bcc1ea4c88dea4
budialim10990 <116984729+budialim10990@users.noreply.github.com>
2022-11-17 16:49:18 +0800

    Add files via upload

---
+++ model.js
@@ -0,0 +1,4 @@
+var random = Math.floor(Math.random() * $('.percent').length);
+$('.percent').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.cas').length);
+$('.cas').hide().eq(random).show();
